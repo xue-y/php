dd95850f6ac68a02d3277374395ef3703645563b
xue-y <1922527784@qq.com>
2018-09-12 18:06:51 +0800

    del cache files

--- fenxiao/Application/Runtime/Data/_fields/back.self_limit.php
+++
@@ -1,1 +0,0 @@
-a:6:{i:0;s:2:"id";i:1;s:1:"n";i:2;s:5:"execs";i:3;s:3:"pid";s:3:"_pk";s:2:"id";s:5:"_type";a:4:{s:2:"id";s:19:"tinyint(4) unsigned";s:1:"n";s:11:"varchar(30)";s:5:"execs";s:12:"varchar(100)";s:3:"pid";s:19:"tinyint(4) unsigned";}}
--- fenxiao/Application/Runtime/Data/_fields/back.self_user_fj.php
+++
@@ -1,1 +0,0 @@
-a:7:{i:0;s:2:"id";i:1;s:5:"phone";i:2;s:2:"wx";i:3;s:4:"info";i:4;s:3:"meg";s:3:"_pk";s:2:"id";s:5:"_type";a:5:{s:2:"id";s:29:"smallint(5) unsigned zerofill";s:5:"phone";s:11:"varchar(11)";s:2:"wx";s:11:"varchar(40)";s:4:"info";s:12:"varchar(200)";s:3:"meg";s:10:"tinyint(4)";}}
